61a0619da32796b741ad3a78d6a9a8d78427b956
Filipe <filipecandido123@hotmail.com>
2021-05-24 19:10:58 -0300

    objects no javascript

---
+++ exercises/bloco_4/dia_4/object.js
@@ -0,0 +1,31 @@
+// let person = {
+//   name: "milton",
+//   lastName: "Nascimento",
+//   age: 45,
+//   bestAlbuns: [ "Travessia", "Clube da esquina", "Minas"]
+// }
+
+// person["fullName"] = person["name"] + ' ' + person["lastName"];
+
+// console.table(person)
+
+
+
+let player = {
+  name: "Marta",
+  lastName: "Silva",
+  age: 34,
+  medals: {
+    golder: 2,
+    silver: 3
+  }
+}
+
+let bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018]
+
+//console.log("A jogadora", player["name"], player["lastName"], "tem", player["age"], "anos de idade.")
+
+// console.log("A jogadora", player["name"], player["lastName"], "foi eleita a melhor jogadora do mundo por", bestInTheWorld.length, "vezes." )
+
+// console.log("A jogadora possui", player.medals.golder, "medalhas de ouro é", player.medals.silver, "medalhas de prata");
+
