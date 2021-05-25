62ce32f9dcb6ef2812a28adad1f146f7d77cd5c2
Filipe <filipecandido123@hotmail.com>
2021-05-25 13:54:59 -0300

    parte_2_4_functions

--- exercises/bloco_4/dia_4/part_2_4_Functions.js
+++ exercises/bloco_4/dia_4/part_2_4_Functions.js
@@ -1,21 +1,26 @@
 /*5- Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
 */
 
-let array = [2, 3, 2, 5, 8, 2, 3];
-let counter = 0;
-let result = array[0];
-let aux = 0;
+let array = [2, 3, 2, 2, 5, 8, 2, 3];
 
-for(let i = 0 ; 1 < array.length; i += 1){
-    for(let j = 0; j < array.length; j += 1){
-      if(array[i] === array[j]) {
-        aux++;
-      }
+
+function myFunc(arr){
+  let counter = 0;
+  let result = array[0];
+  let aux = 0;
+
+    for (let i = 0; i < array.length; i++){
+      for (let j = 0; j < array.length; j++){
+        if(array[i] === array[j]) {
+          aux++;
+        }
     }
     if(aux > counter) {
-      result = array[i]
-      counter = aux
-      aux = 0
+      result = array[i];
+      counter = aux;
+      aux = 0;
     }
  }
- console.log(result)
+  return result
+}
+console.log(myFunc(array))
