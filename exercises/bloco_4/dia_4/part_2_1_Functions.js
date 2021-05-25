a108c686fb60eb6e8ac67c935bc62d858d3cee0e
Filipe <filipecandido123@hotmail.com>
2021-05-25 03:29:15 -0300

    parte 2.1 functions

--- exercises/bloco_4/dia_4/part_2_1_Functions.js
+++ exercises/bloco_4/dia_4/part_2_1_Functions.js
@@ -2,9 +2,22 @@
 Array de teste: [2, 3, 6, 7, 10, 1]; .
 Valor esperado no retorno da função: 4 .*/ 
 
-function inteiroArray(valor) {
-  let ArrayTest = [2, 3, 6, 7, 10, 1];
-  let max = Math.max(...valor)
+// function inteiroArray(valor) {
+//   return Math.max(...valor)
+// }
+// let testArray = [2, 3, 6, 7, 10, 1];
+// console.log(inteiroArray(testArray))
 
-  return inteiroArray(valor)
+// function inteiroArray() {
+  
+// }
+function arrayInteiros(valor) {
+  let maiorValor = 0;
+  for(let key in valor) {
+    if(valor[maiorValor] < valor[key]){
+      maiorValor = key
+    }
+  }
+  return maiorValor
 }
+console.log(arrayInteiros([2, 3, 6, 7, 10, 1]))
