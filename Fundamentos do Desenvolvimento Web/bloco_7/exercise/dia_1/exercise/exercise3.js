3c47b2d148c74e03b92bb09931f122c8e56c6215
Filipe <filipecandido123@hotmail.com>
2021-06-22 01:05:17 -0300

    ocorreu um bug delete a pasta

--- Fundamentos do Desenvolvimento Web/bloco_7/exercise/dia_1/exercise/exercise3.js
+++
@@ -1,7 +0,0 @@
-let button = document.getElementById("clickme")
-let span = document.getElementById("contador"),
-  count = 0;
-button.onclick = () => {
-  count += 1;
-  span.innerHTML = `Número: ${count}`
-};
