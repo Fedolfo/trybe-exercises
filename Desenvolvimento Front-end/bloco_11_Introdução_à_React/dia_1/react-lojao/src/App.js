dadca2e85466b90824b6690337506482568e4961
Filipe <filipecandido123@hotmail.com>
2021-07-20 03:08:12 -0300

    dia 1 finished

---
+++ Desenvolvimento Front-end/bloco_11_Introdução_à_React/dia_1/react-lojao/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NameUser from './Component';
+function App() {
+  return (
+    <NameUser />
+  );
+}
+
+export default App;
---
+++ Desenvolvimento Front-end/bloco_11_Introdução_à_React/dia_1/react-lojao/src/Component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NameUser extends Component {
+  render() {
+    return <div>
+      <h1>
+        <p>Filipe Cândido do Amaral</p>
+      </h1>
+    </div> 
+  }
+};
+
+export default NameUser;
