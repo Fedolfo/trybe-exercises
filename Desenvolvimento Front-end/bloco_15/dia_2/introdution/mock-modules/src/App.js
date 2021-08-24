83590ba8ec8fcab19e3ee29f04c611c3fa2f6c01
Filipe <filipecandido123@hotmail.com>
2021-08-24 01:13:40 -0300

    dia 1 bloco 16

---
+++ Desenvolvimento Front-end/bloco_15/dia_2/introdution/mock-modules/src/App.js
@@ -0,0 +1,56 @@
+// App.js
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      joke: '',
+      nome: '',
+      email: '',
+    }
+  }
+
+  componentDidMount() {
+    const API_URL = 'https://icanhazdadjoke.com/';
+    fetch(API_URL, { headers: { Accept: 'application/json' } })
+      .then((response) => response.json())
+      .then((data) => this.setState({ joke: data.joke }));
+  }
+
+  handleInput(e) {
+    const { name, value } = e.target;
+    console.log(name, value);
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.joke}
+        <h1>Teste de inputs</h1>
+        <p>
+          Nome:
+          <input
+            onChange={(e) => this.handleInput(e)}
+            name='nome'
+            value={this.state.nome}
+            data-testid='input-nome'
+          />
+        </p>
+        <p>
+          E-mail:
+          <input
+            onChange={(e) => this.handleInput(e)}
+            name='email'
+            value={this.state.email}
+            data-testid='input-email'
+          />
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ Desenvolvimento Front-end/bloco_16/dia_1/introdution/combinersReduce.js
@@ -0,0 +1,34 @@
+// Arquivo index.js
+import { combineReducers } from 'redux';
+import meuReducer from './meuReducer';
+import meuOutroReducer from './meuOutroReducer';
+
+const reducerCombinado = combineReducers({
+meuReducer,
+meuOutroReducer});
+
+export default reducerCombinado;
+
+
+import { createStore } from 'redux';
+// Importando o reducer combinado que fizemos logo acima
+import reducerCombinado from './reducers/index';
+
+const store = createStore(reducerCombinado);
+
+console.log(store.getState())
+
+//{
+// meuReducer: {/_estado do meuReducer_/},
+// meuOutroReducer: {/_estado do meuOutroReducer_/,}
+//}
+
+import { createStore } from 'redux';
+// Importando o reducer combinado que fizemos logo acima
+import reducerCombinado from './reducers/index';
+
+const store = createStore(reducerCombinado);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
